b8302fb39a6ff0979f86927321babfef40e5abba
medAziz <med.aziz053@gmail.com>
2018-06-24 09:17:44 +0100

    integration-6

--- app/amortissement/amortissement.js
+++ app/amortissement/amortissement.js
@@ -12,8 +12,21 @@ angular.module('myApp.amortissement', ['ngRoute'])
 .controller('AmortissementCtrl', function($scope, $http) {
 	var months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
 	$scope.chercher = () => {
-		var date1 = $scope.echeance.split('-')[0];
-		var date2 = $scope.echeance.split('-')[1];
+		var date1 = $scope.echeance.split('-')[0] || null;
+		var date2 = $scope.echeance.split('-')[1] || null;
+		var montant = $scope.montant || -1;
+		var matricule = $scope.matricule || null;
+
+		var data = {matricule, date1, date2, montant};
+
+		$http.post('http://localhost:8080/amortissementservice/amortissements/filtrage', {
+
+		}).then(function(response) {
+        	$scope.amortissements = response;
+        	$scope.amortissements.forEach((am) => {
+        		am.moisString = months[am.mois + 1];
+        	});
+        });
 		var result = null;
 	}
 
