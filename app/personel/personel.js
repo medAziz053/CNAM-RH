9c106e46004e1fbf83a8e38778b26fb9e4810a3f
medAziz <med.aziz053@gmail.com>
2018-06-24 01:52:44 +0100

    add edit and add personel

--- app/personel/personel.js
+++ app/personel/personel.js
@@ -6,11 +6,20 @@ angular.module('myApp.personel', ['ngRoute'])
   $routeProvider.when('/personel', {
     templateUrl: 'personel/personel.html',
     controller: 'PersonelCtrl'
-  });
+  })
+  .when('/personel/edit', {
+  	templateUrl: 'personel/editPersonel.html',
+  	controller: 'editPersonelController'
+  })
+  .when('/personel/add', {
+  	templateUrl: 'personel/editPersonel.html',
+  	controller: 'addPersonelController'
+  })
 }])
 
-.controller('PersonelCtrl', function($scope) {
+.controller('PersonelCtrl', function($scope, $rootScope, $location) {
 	$scope.personel = [{
+						'id': 1,
 						'name': 'Bouhammi Sami',
 			     		'birth': '29/01/1990',
 			      		'registrationNumber':201250,
@@ -21,6 +30,7 @@ angular.module('myApp.personel', ['ngRoute'])
 			      		'position': 'Actif'
 			      	  },
 			      	  {
+			      	  	'id': 2,
 						'name': 'Bouhammi Sami',
 			     		'birth': '29/01/1990',
 			      		'registrationNumber':201250,
@@ -31,6 +41,7 @@ angular.module('myApp.personel', ['ngRoute'])
 			      		'position': 'Actif'
 			      	  },
 			      	  {
+			      	  	'id': 3,
 						'name': 'Bouhammi Sami',
 			     		'birth': '29/01/1990',
 			      		'registrationNumber':201250,
@@ -41,6 +52,7 @@ angular.module('myApp.personel', ['ngRoute'])
 			      		'position': 'Actif'
 			      	  },
 			      	  {
+			      	  	'id': 4,
 						'name': 'Bouhammi Sami',
 			     		'birth': '29/01/1990',
 			      		'registrationNumber':201250,
@@ -50,4 +62,52 @@ angular.module('myApp.personel', ['ngRoute'])
 			      		'childenCount': 10,
 			      		'position': 'Actif'
 			      	  }];
+
+	$scope.addPersonel = () => {
+		$location.path('/personel/add');
+	}
+
+	$scope.removePersonel = function(id) {
+		console.log('remove');
+	}
+
+	$scope.editPersonel = function(personel) {
+		$location.path('/personel/edit');
+		$rootScope.personelToEdit = personel;
+	}
+
+})
+.directive('ngConfirmClick', [
+    function(){
+        return {
+            link: function ($scope, element, attr) {
+                var msg = attr.ngConfirmClick || "Are you sure?";
+                var clickAction = attr.confirmedClick;
+                element.bind('click',function (event) {
+                    if ( window.confirm(msg) ) {
+                        $scope.$eval(clickAction)
+                    }
+                });
+            }
+        };
+}])
+.controller('editPersonelController', function($scope, $rootScope, $location) {
+	$scope.personel = $rootScope.personelToEdit;
+	$rootScope.personelToEdit = null;
+	$scope.cancel = function () {
+		$location.path('/personel');
+	}
+	$scope.save = () => {
+		//update
+		$location.path('/personel');
+	}
+})
+.controller('addPersonelController', function($scope, $location) {
+	$scope.cancel = function () {
+		$location.path('/personel');
+	}
+	$scope.save = () => {
+		//update
+		$location.path('/personel');
+	}	
 });
